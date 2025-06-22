682e453442b5ae7afceca57e2bba82580f99b229
rahul <rahulraajesh138@gamil.com>
2025-06-22 18:50:04 +0530

    firstCommit

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+
+
+function showSidebar(){
+    const sidebar= document.querySelector('.mobile_sidebar')
+    sidebar.style.display='flex'
+}
+
+function closeSidebar(){
+    const sidebar=document.querySelector('.mobile_sidebar')
+    sidebar.style.display='none'
+}
