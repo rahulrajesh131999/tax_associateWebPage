bd0fc69d81b181200882532c6165657fea397803
rahul <rahulraajesh138@gamil.com>
2025-06-22 22:47:28 +0530

    finalCommit

--- src/js/script.js
+++ src/js/script.js
@@ -3,9 +3,11 @@
 function showSidebar(){
     const sidebar= document.querySelector('.mobile_sidebar')
     sidebar.style.display='flex'
+
 }
 
 function closeSidebar(){
     const sidebar=document.querySelector('.mobile_sidebar')
     sidebar.style.display='none'
+
 }
